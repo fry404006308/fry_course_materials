7668e3eee4a05aad3aed1a7663ce70d4b1821b7a
fryInShiFang <404006308@qq.com>
2024-03-27 07:42:49 +0800

    _240327_0742_

--- 240206_Yolov8/ultralytics_fry/_Part03_SelfDataset/_02_TrainDetect/420_Detect_Train.py
+++ 240206_Yolov8/ultralytics_fry/_Part03_SelfDataset/_02_TrainDetect/420_Detect_Train.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 
     # Train the model
-    results = model.train(data='FryCoco128_4060.yaml',project='./', epochs=100, imgsz=640)
+    results = model.train(data='FryCoco128_4060.yaml',project='./', epochs=200, imgsz=640)
 
 
 
